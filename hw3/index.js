b732df44eb685fc08f89da5bfae781f97f1f5764
m-sokolskiy <rognarius@icloud.com>
2023-06-15 10:23:20 +0300

    first commit

---
+++ hw3/index.js
@@ -0,0 +1,116 @@
+// // Задание 1
+// let password = prompt('Введите пароль');
+
+// if (password === '123') {
+//     console.log('Пароль введен верно');
+// } else {
+//     console.log('Пароль введен неправильно');
+// }
+
+// // Задание 2
+// let c = 5;
+
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// // Задание 3
+// let d = 33;
+// let e = 44;
+
+// if (d > 100 || e > 100) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// // Задание 4
+// let a = '2';
+// let b = '3';
+// console.log(10 - a - b); 
+// // Я не знаю могу ли я использовать в этом задании дополнительные цифры.
+
+// // Задание 5
+// let monthNumber = prompt('Введите порядковый номер месяца');
+
+// switch (monthNumber) {
+//     case '1':
+//         console.log('Зима');
+//         break;
+//     case '2':
+//         console.log('Зима');
+//         break;
+//     case '3':
+//         console.log('Весна');
+//         break;       
+//     case '4':
+//         console.log('Весна');
+//         break;
+//     case '5':
+//         console.log('Весна');
+//         break;
+//     case '6':
+//         console.log('Лето');
+//         break; 
+//     case '7':
+//         console.log('Лето');
+//         break;
+//     case '8':
+//         console.log('Лето');
+//         break;
+//     case '9':
+//         console.log('Осень');
+//         break;       
+//     case '10':
+//         console.log('Осень');
+//         break;
+//     case '11':
+//         console.log('Осень');
+//         break;
+//     case '12':
+//         console.log('Зима');
+//         break;
+//     default:
+//         console.log('Ошибка');
+//         break;
+// }
+
+// // Задание 7
+// let number = Number(prompt('Пожалуйста, введите любое число'));
+
+// if (number % 2 === 0) {
+//     alert('Четное');
+// } else if (isNaN(number)) {
+//     alert ('NaN');
+// } else {
+//     alert('Нечетное');
+// }
+
+// // Задание 8
+// let clientOS = '1' || '0';
+// clientOS = clientOS.toLocaleLowerCase();
+
+// if (clientOS == 1) {
+//     console.log('Установите версию приложения для Android по ссылке');
+// } else if (clientOS == 0) {
+//     console.log('Установите версию приложения для iOS по ссылке')
+// } else {
+//     console.log('Вы ввели неверную операционную систему');
+// }
+
+// Задание 9
+// let os = '0' || '1';
+// let clientDeviceYear = 2015; 
+// let yearOfIssue = 2014;
+
+// if (yearOfIssue > clientDeviceYear) {
+//     console.log('Ваша ссылка на скачивание приложения');
+// } else if (yearOfIssue < clientDeviceYear && os == 1) {
+//     console.log(`Установите облегченную версию приложения для Android по ссылке`);
+// } else {
+//     console.log(`Установите облегченную версию приложения для iOS по ссылке`);
+// }
+
+
