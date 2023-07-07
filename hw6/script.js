1859c3dab09586597f30b9f59f9cec3fe2fbed77
m-sokolskiy <rognarius@icloud.com>
2023-07-07 16:13:45 +0300

    hw6

---
+++ hw6/script.js
@@ -0,0 +1,104 @@
+// // Задание 1
+// const numbers = [1, 5, 4, 10, 0, 3];
+
+// for (let i = 0; i < numbers.length; i++) {
+// 	if (numbers[i] == 10) break;
+// 	console.log(numbers[i]);
+// }
+
+// // Задание 2
+// const numbers = [1, 5, 4, 10, 0, 3];
+
+// console.log(numbers.indexOf(4));
+
+// // Задание 3
+// let numbers = [1, 3, 5, 10, 20];
+
+// numbers = numbers.join(' ');
+// console.log(numbers);
+
+// // Задание 4
+// let arr = [];
+// for (let i = 0; i < 3; i++){
+//     arr[i] = [];
+
+//     for (let j = 0; j < 3; j++){
+//         arr[i][j] = 1;
+//     }
+// }
+// console.log(arr);
+
+// // Задание 5
+// let numbers = [1, 1, 1];
+
+// numbers.push(2, 2, 2)
+// console.log(numbers);
+
+// // Задание 6
+// let numbers = [9, 8, 7, 'a', 6, 5];
+
+// numbers = numbers.sort();
+// console.log(numbers);
+// let number = numbers.pop();
+// console.log(numbers);
+
+// // Задание 7
+// let arr = [9, 8, 7, 6, 5];
+// let answer = Number(prompt('Угадай число'));
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr.includes(answer)) {
+//         alert('Угадал');
+//         break;
+//     } else {
+//        alert('Не угадал');
+//     }
+// }
+
+
+// // Задание 8
+// let line = 'abcdef';
+// let arrayLine = line.split('').reverse().join('');
+// console.log(arrayLine)
+
+// // Задание 9
+// let numbers = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+// ];
+
+// let result = [].concat(numbers[0],numbers[1]);
+// console.log(result);
+
+// // Задание 10
+// const numbers = [2, 4, 6, 8]
+
+// for (let i = 0; i < numbers.length - 1; i++) {
+//     let sum = numbers[i] + numbers[i + 1];
+//     console.log(sum);
+    
+// }
+
+// // Задание 11
+// const numbers = [2, 4, 6, 8]
+// const result = numbers.map(number => (number**2));
+// console.log(result);
+
+// // Задание 12
+// function lengthSrt(arr) {
+//     return arr.map(el => el.length);
+// }
+
+// console.log(lengthSrt(['Один', '', 'Девять', 'Пять', 'Четыре']));
+
+// // Задание 13
+// function filterPositive(arr) {
+//     let arrIN = arr.filter(el => el < 0) 
+//         console.log(arrIN);
+// }
+
+// filterPositive([-404, 202, 505, -101, -707]);
+
+
+
+
